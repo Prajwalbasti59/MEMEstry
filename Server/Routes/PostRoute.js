58c8a934c560ccb9bb6c4b7c36c8901ee524019a
Prajwalbasti59 <prajwalbasti59@gmail.com>
2023-04-16 14:02:22 +0530

    server updates

--- Server/Routes/PostRoute.js
+++ Server/Routes/PostRoute.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { createPost } from "../Controllers/PostController";
 const router = express.Router()
 
 
-
+router.post
+('/',createPost)
 
 export default router;
+x
